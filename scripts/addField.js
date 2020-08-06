4236b84f4e3dd64fa3f022eb3f5d1d234aad6c9c
JorgeZarlenga <jorge.zarlenga@outlook.com>
2020-08-06 17:15:59 -0300

    Limpeza de campos para duplicacao

--- scripts/addField.js
+++ scripts/addField.js
@@ -7,10 +7,21 @@ document.querySelector("#add-time")
 
 function cloneField(){
     // Duplicar os campos. Que campo duplicar?
-    const fields = document.querySelector('.schedule-item').cloneNode(true)//Clona o campo (Node se refere a elementos HTML)
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)//Clona o campo (Node se refere a elementos HTML)
                                                             // true faz a cópia de toda a div. false copia apenas a div vazia
+
+    // Limpar os campos antes de copiar:
+    const fields = newFieldContainer.querySelectorAll('input') // Associa a todos os seletores input
+
+    // Definindo valores vazios para os campos
+    // Para cada campo, limpar
+    fields.forEach(function(field) {
+        // Pegar o field do momento e o limpa
+        field.value = ""
+    })
+
     // Colocar na página: onde?
-    document.querySelector('#schedule-items').appendChild(fields)
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
 
 
 }
